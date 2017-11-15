26ff8d5bdfc20cd7cc1c817acacb83131c118dd7
valeria <valeriairaismeneses@gmail.com>
2017-11-15 12:14:00 -0600

    part-03 views with ejs

---
+++ src/routers/pageRouter.js
@@ -0,0 +1,17 @@
+const Router = require('express').Router;
+const fs = require('fs-extra');
+
+const pageRouter = Router();
+
+pageRouter
+.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+pageRouter
+.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
+
+
+module.exports = pageRouter;
